ff512f1c58ff2e006df6c64f5926a763f6d2c286
timygonzalez <timygonzalez@gmail.com>
2025-11-29 22:15:20 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/lonedogstudios/',
   plugins: [react()],
-  base: './', // Vital for GitHub Pages: ensures assets load from the correct relative path
 });
